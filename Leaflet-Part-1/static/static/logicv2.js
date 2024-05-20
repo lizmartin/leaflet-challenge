544de97872a6284289542db035bcb0f0517d79ac
Liz Martin-Strong <liz.martin0518@gmail.com>
2024-05-20 09:10:08 -0700

    what a mess

--- Leaflet-Part-1/static/static/logicv2.js
+++ Leaflet-Part-1/static/static/logicv2.js
@@ -1,5 +1,16 @@
+// Create a map object
+let eqMap = L.map("map", {
+    center: [37.09, -95.71], //coordinates for US
+    zoom: 5,
+});
+
+// Add tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(eqMap);
+
 // read in json data file samples.json
-const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
 const dataPromise = d3.json(url);
 console.log("Data Promise: ", dataPromise);
 
@@ -34,8 +45,8 @@ dataPromise.then(function(data) {
             radius: magnitude * 50000
         })
                 
-        //marker.bindPopup("<h3>" + place + "</h3><hr><p>" + "Magnitude: " + magnitude + "</p><p>" + "Time: " + time + "</p>");
-            //quakeMarker.push(marker);
+        marker.bindPopup("<h3>" + place + "</h3><hr><p>" + "Magnitude: " + magnitude + "</p><p>" + "Time: " + time + "</p>");
+            quakeMarker.push(marker);
     
             marker.bindTooltip("<h3>" + `magnitude: ` + magnitude 
             // + "</h3><hr><p>" + `felt?: ` + felt + "</p>"
@@ -54,39 +65,44 @@ dataPromise.then(function(data) {
     // create layer group for the markers.
     var quakes = L.layerGroup(quakeMarker);
 
+    let earthquakes = L.geoJSON(earthquakeData, {
+        quakeData: quakeData,
+        quakeMarker: quakes
+    }).addTo(eqMap);
+
     //define map obj.
-    var myMap = L.map("map", {
-        center: [37.09, -95.71],
-        zoom: 5,
-        layers: [street, quakes]
-    });
+    //var myMap = L.map("map", {
+        //center: [37.09, -95.71],
+        //zoom: 5,
+        //layers: [street, quakes]
+    //});
 
     //create legend
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-        var div = L.DomUtil.create("div", "info legend");
-        var limits = [-10, 10, 30, 50, 70, 90];
-        var colors = ["purple", "blue", "green", "yellow", "orange", "red"];
-        var labels = ["<10", "10-30", "30-50", "50-70", "70-90", ">90"];
+    //var legend = L.control({ position: "bottomright" });
+    //legend.onAdd = function() {
+        //var div = L.DomUtil.create("div", "info legend");
+        //var limits = [-10, 10, 30, 50, 70, 90];
+        //var colors = ["purple", "blue", "green", "yellow", "orange", "red"];
+        //var labels = ["<10", "10-30", "30-50", "50-70", "70-90", ">90"];
 
-        var legendInfo = "<h1>Earthquake Magnitude</h1>" + "<h3>Magnitude</h3>"
-        div.innerHTML = legendInfo;
-        for (var i = 0; i < limits.length; i++) {
-            div.innerHTML +=
-              '<div class="color-box" style="background-color:' + colors[i] + '"></div>' +
-              '<span>' + labels[i] + '</span><br>';
+        //var legendInfo = "<h1>Earthquake Magnitude</h1>" + "<h3>Magnitude</h3>"
+        //div.innerHTML = legendInfo;
+        //for (var i = 0; i < limits.length; i++) {
+            //div.innerHTML +=
+              //'<div class="color-box" style="background-color:' + colors[i] + '"></div>' +
+              //'<span>' + labels[i] + '</span><br>';
 
 
         //for (var i = 0; i < limits.length; i++) {
             //div.innerHTML +=
                 //"<i style='background: " + colors[i] + "'></i> " +
                 //limits[i] + (limits[i + 1]? "&ndash;" + limits[i + 1] + "<br>" : "+");
-        }
+        //}
 
-        return div;
-    };
+        //return div;
+    //};
 
     //add legend to map
-    legend.addTo(myMap);
+    //legend.addTo(myMap);
 
-});
+}
